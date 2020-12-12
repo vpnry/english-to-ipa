558bff49f48238fd2bc31ea0e8973c9d3e477c60
PNRY <pnrynow@gmail.com>
2020-12-12 03:57:44 +0000

    new initial

---
+++ README.md
@@ -0,0 +1,69 @@
+# to-ipa.py
+
+Convert English text to IPA using `python3` and Python module [eng_to_ipa](https://pypi.org/project/eng-to-ipa/)
+
+## Install
+
++ System requirements: `python3`, and module `eng_to_ipa`
++ On Ubuntu, install Python3 with:
+
+```bash
+sudo apt update
+sudo apt install python3
+```
+
+And **eng_to_ipa** module:
+
+```bash
+pip3 install eng_to_ipa
+```
+
+## Usase
+
++ In Terminal CLI:
+
+```bash
+# insert IPA paragraphs follows English paragraphs
+python3 to-ipa.py test.txt 1
+
+# insert IPA transcription next to each word
+python3 to-ipa.py test.txt 0
+```
+
++ For function call: 
+
+```python3
+toIPA(f, 1) # insert IPA paragraphs follows English paragraphs
+toIPA(f, 0) # insert IPA transcription next to each word
+```
+
+## Example
+
++ Input file `input.txt`:
+
+```text
+Adapted from https://en.m.wikipedia.org/wiki/Computer
+A computer is a machine that can be instructed to carry out sequences of arithmetic or logical operations automatically via computer programming. 
+
+Modern computers have the ability to follow generalized sets of operations, called programs. These programs enable computers to perform an extremely wide range of tasks.
+```
+
++ Output `_ipa_input.txt` (in current directory)
+
+```text
+[1] Adapted from https://en.m.wikipedia.org/wiki/Computer
+
+[1] əˈdæptɪd frəm https://en.m.wikipedia.org/wiki/computer*
+
+[2] A computer is a machine that can be instructed to carry out sequences of arithmetic or logical operations automatically via computer programming.
+
+[2] ə kəmˈpjutər ɪz ə məˈʃin ðət kən bi ˌɪnˈstrəktɪd tɪ ˈkɛri aʊt ˈsikwənsɪz əv ˌɛrɪθˈmɛtɪk ər ˈlɑʤɪkəl ˌɑpərˈeɪʃənz ˌɔtəˈmætɪkli ˈviə kəmˈpjutər ˈproʊˌgræmɪŋ.
+
+[3] 
+
+[3] 
+
+[4] Modern computers have the ability to follow generalized sets of operations, called programs. These programs enable computers to perform an extremely wide range of tasks.
+
+[4] ˈmɑdərn kəmˈpjutərz hæv ðə əˈbɪləˌti tɪ ˈfɑloʊ ˈʤɛnərəˌlaɪzd sɛts əv ˌɑpərˈeɪʃənz, kɔld ˈproʊˌgræmz. ðiz ˈproʊˌgræmz ɪˈneɪbəl kəmˈpjutərz tɪ pərˈfɔrm ən ɪkˈstrimli waɪd reɪnʤ əv tæsks.
+```
---
+++ to-ipa.py
@@ -0,0 +1,80 @@
+# Convert English text to IPA using: eng_to_ipa
+# @pnry - version 0.0.2
+# Last modified: 12 Dec 2020
+
+# Usage: 
+# toIPA(f, 1) # insert IPA paragraphs follows English paragraphs
+# toIPA(f, 0) # insert IPA transcription next to each word
+# In Terminal:
+# python3 ipa.py test.txt 0 
+# python3 ipa.py test.txt 1
+
+
+
+import sys
+args = sys.argv
+
+
+#----------------------------------------------
+import eng_to_ipa as ipa # pip3 install eng_to_ipa
+import time
+
+def toIPA(f, y=1):
+    t1 = time.time()
+    writ = ""
+
+    print("Transcripting "+ f +" to IPA ")
+    print("It may take a while, please wait...")
+    print("")
+    with open(f, 'r') as t:
+        tex = t.read()
+        tex = tex.strip()
+        
+        tex = tex.replace("\n"," p15081553y ")
+        ip = ipa.convert(tex)
+        
+        tex = tex.split(" p15081553y ")
+        ip = ip.split("p15081553y*")
+        para = len(tex)
+
+        print("Total: " +  str(para)+ " paragraphs" )
+        
+        if y == 1:
+            print("Arrange paragraph by paragraph")
+            for i in range(para):
+                print("Processing par no.: " +  str(i+1))
+                writ += "[" + str(i+1) +"] " + tex[i].strip() + "\n\n" + "[" + str(i+1) +"] " + ip[i].strip()
+                writ += "\n\n"
+        ###
+        else:
+            print("Insert IPA next to each word")
+            for i in range(para):
+                print("Processing par no.: " +  str(i+1))
+                enp = tex[i].strip().split(" ")
+                ipp = ip[i].strip().split(" ")
+                for k in range(len(enp)):
+                    writ += enp[k] + "[" + ipp[k] + "]  "
+                writ += "\n\n"
+        ###
+        with open('_ipa_' + f, 'w') as p:
+            p.write(writ)
+        print("")
+        print("#-----------------------")
+        print("Done!")
+        print("Took: " + str((time.time() - t1)) + " seconds")
+        print("#-----------------------")
+        
+#------------------- TEST ---------------------
+# f = "text"
+# f = f + ".txt"
+# toIPA(f, y=1):
+
+#-------------------- CLI ---------------------
+# CLI tips, more here: https://stackoverflow.com/a/65108366
+if __name__ == "__main__":
+  # args: array. 0 = filename
+  # From 1 forward, what ever follows after the filename
+  # (*expand)
+  globals()["toIPA"](*args[1:])
+  
+  
