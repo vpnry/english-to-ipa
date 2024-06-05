137e89a248307bc1c429ca55167668bbf945f888
vpnry <72340971+vpnry@users.noreply.github.com>
2024-06-05 12:49:21 +0530

    Update to-ipa.py

--- to-ipa.py
+++ to-ipa.py
@@ -1,84 +1,77 @@
 # Convert English text to IPA using: eng_to_ipa
-# @pnry - version 0.0.2
+# @pnry - version 0.0.3
 # Last modified: 12 Dec 2020
+# Last modified: 05 Jun 2024
 
-# Usage: 
-# toIPA(f, 1) # insert IPA paragraphs follows English paragraphs
-# toIPA(f, 0) # insert IPA transcription next to each word
+
+# Usage:
+# toIPA(filename, mode="para") # insert IPA paragraphs follows English paragraphs
+# toIPA(filename, mode="word") # insert IPA transcription next to each word
 # In Terminal:
-# python3 to-ipa.py test.txt 0 
-# python3 to-ipa.py test.txt 1
+# python3 to-ipa.py input.txt para
+# python3 to-ipa.py input.txt word
 
-
-
+import os
 import sys
-args = sys.argv
-
-
-#----------------------------------------------
-import eng_to_ipa as ipa # pip3 install eng_to_ipa
 import time
+import eng_to_ipa as ipa
 
-def toIPA(f, y=1):
-    t1 = time.time()
-    writ = ""
+NEWLINE_PLACEHOLDER = " p15081553y "
 
-    print("Transcripting "+ f +" to IPA ")
-    print("It may take a while, please wait...")
-    print("")
-    with open(f, 'r') as t:
-        tex = t.read()
-        tex = tex.strip()
-        
-        tex = tex.replace("\n"," p15081553y ")
-        ip = ipa.convert(tex)
-        
-        tex = tex.split(" p15081553y ")
-        ip = ip.split("p15081553y*")
-        para = len(tex)
 
-        print("Total: " +  str(para)+ " paragraphs" )
-        
-        if y == 1:
-            print("Inserting IPA paragraphs follows English paragraphs")
-            for i in range(para):
-                print("Processing par no.: " +  str(i+1))
-                writ += "[" + str(i+1) +"] " + tex[i].strip() + "\n\n" + "[" + str(i+1) +"] " + ip[i].strip()
-                writ += "\n\n"
-        ###
-        else:
-            print("Inserting IPA next to each word")
-            for i in range(para):
-                print("Processing par no.: " +  str(i+1))
-                enp = tex[i].strip().split(" ")
-                ipp = ip[i].strip().split(" ")
-                for k in range(len(enp)):
-                    writ += enp[k] + "[" + ipp[k] + "]  "
-                writ += "\n\n"
-        ###
-        with open('_ipa_' + f, 'w') as p:
-            p.write(writ)
-        print("")
-        print("#-----------------------")
-        print("Done!")
-        print("Took: " + str((time.time() - t1)) + " seconds")
-        print("#-----------------------")
-        
-#------------------- TEST ---------------------
+def toIPA(filename, mode="word"):
+    print(filename, mode)
 
-#-------------------- CLI ---------------------
-# CLI tips, more here: https://stackoverflow.com/a/65108366
+    start_time = time.time()
+    transcript = ""
+
+    print(f"Transcribing {filename} to IPA...")
+    print("This may take a while, please wait...\n")
+
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            text = file.read().strip().split("\n")
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return
+
+    num_paragraphs = len(text)
+
+    print(f"Total: {num_paragraphs} paragraphs")
+
+    if mode == "para":
+        print("Inserting IPA paragraphs following English paragraphs")
+        for i in range(num_paragraphs):
+            print(f"Processing paragraph number: {i+1}")
+            ipa_text = ipa.convert(text[i])
+            transcript += f"[{i+1}] {text[i]}\n\n[{i+1}] {ipa_text}\n\n"
+    else:
+        print("Inserting IPA next to each word")
+        for i in range(num_paragraphs):
+            print(f"Processing paragraph number: {i+1}")
+            english_words = text[i].split(" ")
+            ipa_words = [ipa.convert(word) for word in english_words]
+            for word, ipa_word in zip(english_words, ipa_words):
+                transcript += f"{word}[{ipa_word}]  "
+            transcript += "\n\n"
+
+    base_dir = os.path.dirname(filename)
+    base_filename = os.path.basename(filename)
+    ipa_filename = os.path.join(base_dir, "_ipa_done_" + base_filename)
+
+    with open(ipa_filename, "w", encoding="utf-8") as file:
+        file.write(transcript)
+
+    print("\n#-----------------------")
+    print("Done!")
+    print(f"Took: {time.time() - start_time} seconds")
+    print("#-----------------------")
+
 
 if __name__ == "__main__":
-  # args: array. 0 = filename
-  # From 1 forward, what ever follows after the filename
-  # (*expand)
-  if len(args[1:]) > 0:
-    print("Usong CLI mode")
-    globals()["toIPA"](*args[1:])
-  else:
-    print("Using function call")
-    toIPA("input.txt", 1)
-    
-
-  
+    if len(sys.argv[1:]) > 0:
+        print("Using CLI mode")
+        toIPA(*sys.argv[1:])
+    else:
+        print("Using function call")
+        toIPA("input.txt", "para")
